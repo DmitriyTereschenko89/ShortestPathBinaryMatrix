c92927c63e159bc01984dc3a344ac340d110b4bf
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-06-01 10:21:26 +0300

    update solution and read.me

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@
 		{
 			private List<int[]> GetNeighbors(int[][] grid, int r, int c, int distance, int n)
 			{
+				int[] deltaR = new int[] { 0, -1, -1, -1, 0, 1, 1, 1 };
+				int[] deltaC = new int[] { -1, -1, 0, 1, 1, 1, 0, -1 };
 				List<int[]> neighbors = new();
-				int[] deltaRow = new int[] { 0, -1, -1, -1, 0, 1, 1, 1 };
-				int[] deltaCol = new int[] { -1, -1, 0, 1, 1, 1, 0, -1 };
 				for (int i = 0; i < 8; ++i)
 				{
-					int newR = deltaRow[i] + r;
-					int newC = deltaCol[i] + c;
+					int newR = deltaR[i] + r;
+					int newC = deltaC[i] + c;
 					if (newR >= 0 && newR < n && newC >= 0 && newC < n && grid[newR][newC] == 0)
 					{
 						neighbors.Add(new int[] { newR, newC, distance + 1 });
@@ -23,29 +23,29 @@
 
 			public int ShortestPathBinaryMatrix(int[][] grid)
 			{
-				if (grid[0][0] == 1)
+				int n = grid.Length;
+				if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
 				{
 					return -1;
 				}
-				int n = grid.Length;
 				Queue<int[]> queue = new();
 				queue.Enqueue(new int[] { 0, 0, 1 });
-				while(queue.Count > 0)
+				while (queue.Count > 0)
 				{
-					int queueSize = queue.Count;
-					while(queueSize-- > 0)
+					int queueCount = queue.Count;
+					while (queueCount-- > 0)
 					{
 						int[] cell = queue.Dequeue();
-						List<int[]> neighbors = GetNeighbors(grid, cell[0], cell[1], cell[2], n); 
+						if (cell[0] == n - 1 && cell[1] == n - 1)
+						{
+							return cell[2];
+						}
+						List<int[]> neighbors = GetNeighbors(grid, cell[0], cell[1], cell[2], n);
 						foreach (int[] neighbor in neighbors)
 						{
 							int r = neighbor[0];
 							int c = neighbor[1];
 							int distance = neighbor[2];
-							if (r == n - 1 && c == n - 1)
-							{
-								return distance;
-							}
 							grid[r][c] = 1;
 							queue.Enqueue(new int[] { r, c, distance });
 						}
